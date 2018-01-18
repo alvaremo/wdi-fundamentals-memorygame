490122166db4138cef600ae9fe893da6e63c14c7
alvaremo <monica.alvarez@espn.com>
2018-01-17 19:50:26 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cards = [ "queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[3];
+cardsInPlay.push(cardOne);
+
+
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo)
+
+console.log("User flipped queen.");
+console.log("User flipped king.");
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+ alert("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+  alert("Sorry,try again.");
+}
