8df0c217560f04be723c7ddc8941d531c684a112
alvaremo <monica.alvarez@espn.com>
2018-01-23 01:33:13 -0800

    continued game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,25 +1,49 @@
 
-var cards = [ "queen", "queen", "king", "king"];
-var cardsInPlay = [];
-cardsInPlay.push(cards["queen"])
+var cards = [
+  {
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+  },
+{
+rank: "queen",
+suit: "diamonds",
+cardImage:"images/queen-of-diamonds.png"
+},
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+];
 
-var checkForMatch = function () {
+var cardsInPlay = [];
+
+var checkForMatch = function() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
   console.log("You found a match!");
    alert("You found a match!");
   } else {
     console.log("Sorry, try again.");
     alert("Sorry,try again.");
-};
+  }
+}
+
+var flipCard = function(cardId) {
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("User flipped" + cards[cardId].rank);
+   {
+  checkForMatch();
+  }
+  console.log(cards[cardId].suit);
+  console.log(cards[cardId].cardImage);
 }
 
 
-
-var flipCard = function() {
-  if (flipCard === 1) {
-  console.log("User flipped" + cards["king"]);
-} checkForMatch();
-}
-
-flipCard(0);
 flipCard(2);
+flipCard(1);
